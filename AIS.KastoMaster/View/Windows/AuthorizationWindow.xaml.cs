fe486e28e1de79b25febbb84f89c2a43b5723436
User12 <User12@WIN-LHUU780N9SM>
2025-02-14 13:24:28 +0300

    Сделал поле логина

--- AIS.KastoMaster/View/Windows/AuthorizationWindow.xaml.cs
+++ AIS.KastoMaster/View/Windows/AuthorizationWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Shapes;
 
 namespace AIS.KastoMaster.View.Windows
@@ -19,9 +20,28 @@ namespace AIS.KastoMaster.View.Windows
     /// </summary>
     public partial class AuthorizationWindow : Window
     {
+        int MouseFlag = 0;
         public AuthorizationWindow()
         {
             InitializeComponent();
         }
+
+        private void TextBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(LoginTb.Text == "Введите имя пользователя")
+            {
+                LoginTb.Text = String.Empty;
+            }            
+        }
+
+        private void LoginTb_MouseLeave(object sender, MouseEventArgs e)
+        {
+               if (LoginTb.Text == String.Empty)
+            {
+                LoginTb.Text = "Введите имя пользователя";
+            }
+        }
+
+   
     }
 }
